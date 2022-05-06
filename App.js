35bb7ecd7307e1d4bdcca0a6ac488a0dab01ba4e
Julian Miller <73328279+docjulz@users.noreply.github.com>
2022-05-05 19:28:22 -0700

    File upload
    These files were uploaded from Scrimba

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Buttons from "./Components/Buttons"
+import Header from "./Components/Header"
+import Main from "./Components/Main"
+import Footer from "./Components/Footer"
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Buttons />
+            <Main />
+            <Footer />
+        </> 
+    )    
+}
